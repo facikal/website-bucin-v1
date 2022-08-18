2ec946d553b2f7ba934d184168e4109918adb297
facikal <fadhil1999@gmail.com>
2022-08-19 00:58:53 +0700

    fix ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ hari.innerHTML = `${dayID()}, ${tgl} ${monthID()} ${year}`
 jam.innerHTML = `${hour}:${minutes}`
 
 // Dirubah 
-const pengirim = "";
+const pengirim = "Cikal";
 const nomorWa = "6289668068213" // awalan nomor 0 harus di awalin 62
 const textWa = "Ah bisa aja kamu"
 const pesan = `Pada akhirnya kita berdua menjadi dua orang penjahat: Kamu yang dengan hebatnya bisa mencuri hatiku, begitu juga aku yang berhasil mencuri hatimu`;
